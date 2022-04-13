c06c94b4f44c35abde6fc37e38518a2b279e9c91
Brajesh Kumar Saxena <95098581+brajeshsaxena01@users.noreply.github.com>
2022-04-13 23:45:40 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="electronics">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufucture</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
     </div>
   );
 }
